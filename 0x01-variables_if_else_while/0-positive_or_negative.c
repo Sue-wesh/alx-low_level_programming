2c867a43ebf62659c7cea236ee73ec457c9e6257
Sue-wesh <111245698+Sue-wesh@users.noreply.github.com>
2022-09-10 00:51:38 +0300

    quiz 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -22,13 +22,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if(n>0){
-		Printf("%d\n",n, "is positive");
+		printf ("%d\n",n, "is positive");
 	}
 	else if(n==0){
-		Printf("%d\n",n, "is zero");
+		printf ("%d\n",n, "is zero");
 	}
 	else if(n<0){
-		Printf("%d\n",n, "is negative");
+		printf ("%d\n",n, "is negative");
 	}
 	return (0);
 

551d6196346dfb66c79817162f2165a9881d34a3
Sue-wesh <111245698+Sue-wesh@users.noreply.github.com>
2022-09-10 01:17:46 +0300

    quiz1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,15 +18,17 @@ int main(void)
 
 {
 	int n = 98;
+	int n1 = 0;
+	int n2 = -98
 
 	if ( n > 0 ) {
 		printf ("%d is positive\n", n );
 	}
-	else if ( n ==0 ) {
-		printf ("0 is zero\n", n );
+	else if ( n1 ==0 ) {
+		printf ("%d is zero\n", n1 );
 	}
-	else if ( n < 0 ) {
-		printf ("-%d is negative\n",n);
+	else if ( n2 < 0 ) {
+		printf ("%d is negative\n",n2);
 	}
 	return (0);
 

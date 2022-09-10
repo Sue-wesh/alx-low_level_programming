1f10036e78ddc7ac46f58ac5f8dba18cd99a5173
Sue-wesh <shiroweru007@gmail.com>
2022-09-09 19:38:11 -0700

    Quiz11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+/* betty style doc for function main goes there */
+/**
+ * main - entrypoint
+ *
+ * Return: zero at the end
+ *
+ */
+
+int main(void)
+{
+	int a, b, c;
+	int arr[3];
+
+	printf("Enter the first number : ");
+        scanf("%d", &arr[0]);
+
+        printf("Enter the second number : ");
+        scanf("%d", &arr[1]);
+
+        printf("Enter the third number : ");
+        scanf("%d", &arr[2]);
+
+	for(a = 0; a<3; a++)
+	{
+		for(b = 0; b < 3; b++)
+		{
+			for(c = 0; c < 3; c++)
+				putchar("%d%d%d,", arr[a],arr[b],arr[c]);
+		}
+	}
+	return (0);
+}

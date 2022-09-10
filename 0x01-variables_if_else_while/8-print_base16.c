1427ca9cf813f3470c3c949953d72e0a2f282f1d
Sue-wesh <shiroweru007@gmail.com>
2022-09-09 18:46:04 -0700

    Quiz9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - entrypoint
+ * 
+ * Return: return 0 at end
+ *
+ */
+int main(void)
+{
+	char c;
+
+	if(c % 16)
+	{
+		putchar(c);
+	}
+	return (0);
+}
+

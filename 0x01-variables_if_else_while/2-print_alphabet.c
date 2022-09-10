497056f9ae0365140ecc7fd055cc63f6fadec5a2
Sue-wesh <111245698+Sue-wesh@users.noreply.github.com>
2022-09-10 03:03:27 +0300

    Quiz3

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+/* betty style doc for function main goes there */
+/**
+ * main - entrypoint
+ *
+ * Return: return 0
+ *
+ */
+
 int main(void)
 {
+	int x;
 	for(x = 'a'; x<='z'; x++){
 		putchar(x);
 		return(0);

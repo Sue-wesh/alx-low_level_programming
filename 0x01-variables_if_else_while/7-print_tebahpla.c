8c91dbafeec5527686b4fd635f57be0c4ee9eb8d
Sue-wesh <shiroweru007@gmail.com>
2022-09-09 18:30:54 -0700

    Quiz8

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/* betty style doc for function main goes there */
+/**
+ * main - entrypoint
+ *
+ * Return: return 0 at end
+ *
+ */
+
+int main(void)
+{
+	int k;
+
+	for(k = 'z'; k >= 'a'; k--)
+		putchar(k);
+	return (0);
+}

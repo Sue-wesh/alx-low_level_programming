7c7295177d20a5089ce70e7d9e6681a24615905b
Sue-wesh <shiroweru007@gmail.com>
2022-09-09 19:02:20 -0700

    Quiz10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - combinations of two digits
+ *
+ * Return: return 0 at the end
+ *
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+
+	for(x = 0;x < 10; x++)
+	{
+		for(y = 0;y < 10; y++)
+			putchar(xy,",")
+	}
+	return (0);

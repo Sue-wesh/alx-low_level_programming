d2fc5d675464a9c2ea146317154c69fc3f1e61fa
Sue-wesh <shiroweru007@gmail.com>
2022-09-09 18:22:07 -0700

    Quiz7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/* betty style doc for function main goes there */
+/**
+ * main - entrypoint
+ *
+ * Return: return 0 at the end
+ *
+ */
+
+int main(void)
+{
+	int y;
+
+	for(y = 0; y < 10; y++)
+	{
+		putchar(y);
+	}
+	return (0);
+}

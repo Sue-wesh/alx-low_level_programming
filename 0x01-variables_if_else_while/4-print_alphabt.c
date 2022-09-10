69952837c065660a1062a4c83659b93843c96554
Sue-wesh <shiroweru007@gmail.com>
2022-09-09 17:50:02 -0700

    Quiz5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/* betty style doc for function main goes there */
+/**
+ * main - entrypoint
+ *
+ * Return: return 0
+ *
+ */
+
+int main(void)
+{
+	int x = 'a'
+	
+	do
+	{
+		if(x = q || e)
+			continue;
+		x++;
+	}
+	while(x <= 'z')
+		return (o);

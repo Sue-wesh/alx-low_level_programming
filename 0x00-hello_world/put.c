fd04de4b929e0d5ee4441dc4990d6757218b1f58
Sue-wesh <shiroweru007@gmail.com>
2022-09-12 03:28:51 -0700

    C project

---
+++ 0x00-hello_world/put.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entrypoint for our program
+ *
+ * Description: program that uses puts to return
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

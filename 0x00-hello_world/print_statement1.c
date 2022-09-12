16a54f288c482964d35b51cd026f2287a47312fc
Sue-wesh <shiroweru007@gmail.com>
2022-09-11 21:25:35 -0700

    statement1

---
+++ 0x00-hello_world/print_statement1.c
@@ -0,0 +1,13 @@
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int myVar = "Programming is like building a multilingual puzzle";
+	puts(myVar);
+	return (0);
+}

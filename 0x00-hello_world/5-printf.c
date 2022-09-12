b03feb680bed2c81149066b8e0f6cca0b7f94f3d
Sue-wesh <shiroweru007@gmail.com>
2022-09-12 03:44:01 -0700

    Cprojoct

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - entrypoint
+ *
+ * Description: using printf funcion
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	 printf("with proper grammar, but the outcome is a piece of art,\n");                                                       	     return (0);                                                                                                                }

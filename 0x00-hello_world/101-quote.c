c9dfc806da15ffa71d349aa4bb25d5a1eb3247c9
Sue-wesh <shiroweru007@gmail.com>
2022-09-12 20:12:14 -0700

    c project

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- Entrpoint
+ *
+ * Description: display a statement
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	putchar(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
+

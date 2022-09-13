e61facd925304ad603b02981f8a15bd1c69753a3
Sue-wesh <shiroweru007@gmail.com>
2022-09-12 18:59:08 -0700

    Quiz5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  * main - entrypoint
  *
@@ -11,4 +10,6 @@
 
 int main(void)
 {
-	 printf("with proper grammar, but the outcome is a piece of art,\n");                                                       	     return (0);                                                                                                                }
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

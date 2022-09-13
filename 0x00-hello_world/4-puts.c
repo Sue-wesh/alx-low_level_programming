d952fef148267ada74cd30b95540ec78cfef6ff5
Sue-wesh <shiroweru007@gmail.com>
2022-09-12 18:54:26 -0700

    C is awesome

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-gcc print_statement1.c
+#include <stdio.h>
+
+/**
+ * main - Entrypoint
+ *
+ * Description: program uses puts to print a statement
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

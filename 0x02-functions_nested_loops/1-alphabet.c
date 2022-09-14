2062eab7c87802bcbda02d00ba8150cf5f9a9b1a
Sue-wesh <shiroweru007@gmail.com>
2022-09-14 03:55:52 -0700

    Quiz1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - display the alphabet in lowercase
+ * @a: our parameter
+ * Return: 0
+ */
+int print_alphabet(int a)
+{
+	for (int a == 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entrypoint
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}

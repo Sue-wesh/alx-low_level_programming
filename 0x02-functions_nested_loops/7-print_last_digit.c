4c9738137ac7c00fc3ad6472ad80d5e20f299e0d
Sue-wesh <shiroweru007@gmail.com>
2022-09-14 09:19:52 -0700

    Quiz7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - find the last digit
+ *
+ * @n: our parameter
+ *
+ * Return: 0 at the end
+ */
+int print_last_digit(int n)
+{
+	int y;
+
+	y = n % 10;
+
+	while ((n <= 0) || (n > 0))
+	{
+		_putchar(y);
+	}
+	return (0);
+}

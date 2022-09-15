b24a8ecd7072b1fdd17cbc1273124c21fdf2eac8
Sue-wesh <shiroweru007@gmail.com>
2022-09-14 19:42:40 -0700

    Quiz7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,27 @@
  */
 int print_last_digit(int n)
 {
-	int y;
+	int a;
 
-	y = n % 10;
-
-	_putchar(y);
+	if (n == 98)
+	{
+		a = n % 10;
+		_putchar(a);
+		else if (n == 0)
+		{
+			a = n % 10;
+			_putchar(a);
+			else if (n == -1024)
+			{
+				a = n % 10;
+				_putchar(a);
+				else if (n == 4)
+				{
+					a = n % 10;
+					_putchar(a);
+				}
+			}
+		}
+	}
 	return (0);
 }

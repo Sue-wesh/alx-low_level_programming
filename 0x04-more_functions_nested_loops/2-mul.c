fc8948a551095b7e7fe53e6bb82e5b6fda4673ca
Sue-wesh <shiroweru007@gmail.com>
2022-09-15 16:17:10 -0700

    Quiz2 nested_loop

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiply two integers
+ * @a: parameter first
+ * @b: second parameter
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	int x;
+
+	x = a * b;
+	putchar(x);
+
+	return (x);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,5 +8,6 @@ int toupper(int ch);
 int _isdigit(int c);
 void print_numbers(void);
 int _putchar(char c);
+int mul(int a, int b);
 
 #endif /* MAIN_H */

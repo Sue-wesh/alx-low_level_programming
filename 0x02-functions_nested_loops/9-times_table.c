08b4ec2479f009c6b6909ca4ba39d1968c3ae800
Sue-wesh <shiroweru007@gmail.com>
2022-09-18 08:36:43 -0700

    Quiz9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - display the 9 timestable
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			printf("%d", i * j);
+			if (j < 9)
+			{
+				printf(", ");
+			}
+		}
+		putchar('\n');
+	}
+}

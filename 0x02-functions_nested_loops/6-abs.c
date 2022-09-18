8517bef33c9b9eddd6122065e8833d7492b01919
Sue-wesh <shiroweru007@gmail.com>
2022-09-18 06:19:30 -0700

    Quiz6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,20 +5,17 @@
 /**
  * _abs - absolute value
  *
- * @n: our parameter
- *
  * Return: 0
  */
-int _abs(int n)
+int _abs(int x)
 {
-	if (n < 0)
+	int y;
+
+	if (x < 0)
 	{
-		n = -n;
-		_putchar(abs(n));
-	}
-	else
-	{
-		_putchar(n);
+		x = -x;
+		y = abs(x);
+		putchar(y);
 	}
 	return (0);
 }

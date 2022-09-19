6be60e0ad9a63aed5fb2a66bfc87abae2a39faa9
Sue-wesh <shiroweru007@gmail.com>
2022-09-19 07:30:00 -0700

    C project_1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - functions swaps the value of ints
+ * @a: stores val of var int
+ * @b: stores value of var int
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}

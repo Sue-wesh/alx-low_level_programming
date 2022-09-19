cbcc5dbf68bfebc8ce9436d126be6cdf7241f98b
Sue-wesh <shiroweru007@gmail.com>
2022-09-19 08:01:45 -0700

    C project_2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - the length of a string find
+ * @s: the character we output
+ * Return: the value of x
+ */
+int _strlen(char *s) /* s=&str[0] */
+{
+	int x;
+
+	x = 0;
+	while (*s != '\0')
+	{
+		x++;
+		s++;
+	}
+	return (x);
+}

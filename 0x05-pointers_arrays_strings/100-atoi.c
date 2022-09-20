8e58ac7b29de3071c2f72a6eb7ac9a53dcba15f6
Sue-wesh <shiroweru007@gmail.com>
2022-09-20 09:16:03 -0700

    C project_10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _atoi - convert string to integer
+ * @s: the pointer to the string
+ * Return: the value to be displayed
+ */
+int _atoi(char *s)
+{
+	int x = atoi(s);
+
+	return (x);
+}

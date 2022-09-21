fe54d12b10ffb4b6679c733266184cc1cd912220
Sue-wesh <shiroweru007@gmail.com>
2022-09-20 21:25:24 -0700

    C project_11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - display numbers from n to 98
+ * @n: the character to be displayed to stdout
+ */
+void print_to_98(int n)
+{
+	while (n > 98)
+	{
+		printf("%d", n);
+		n--;
+	}
+	while (n <= 98)
+	{
+		printf("%d", n);
+		n++;
+	}
+	putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -10,3 +15,6 @@ int add(int, int);
 void print_to_98(int n);
 void jack_bauer(void);
 void times_table(void);
+void print_to_98(int n);
+
+#endif /* MAIN_H */

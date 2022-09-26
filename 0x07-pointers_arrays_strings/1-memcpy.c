78fc672edf992d6a9c8af7fd5cbe7972ae153e6d
Sue-wesh <shiroweru007@gmail.com>
2022-09-26 08:56:19 -0700

    Quiz1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,12 +10,6 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned  int i;
-
-	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i] = '\0';
-	return (0);
+	memcpy(dest, src, n);
+	return (memcpy(dest, src, n));
 }

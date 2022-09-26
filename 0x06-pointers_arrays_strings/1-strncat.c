097293289e33bc531ae3b059391370e6a5eb53d3
Sue-wesh <shiroweru007@gmail.com>
2022-09-26 02:45:04 -0700

    Quiz1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,5 +12,13 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	return (strncat(dest, src, n));
+	int i, len;
+
+	len = strlen(dest);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[len + i] = src[i];
+	}
+	dest[len + i] = '\0';
+	return (dest);
 }

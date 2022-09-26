484eca91c8c79d8b9790c2038614e089927e46b6
Sue-wesh <shiroweru007@gmail.com>
2022-09-26 13:05:17 -0700

    get length of aprefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strspn - function gets the length of a substring
+ * with a certain string or character
+ * @s: the string
+ * @accept: the string we search in string s and calculate
+ * the substring with this string
+ * Return: the end result
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int len;
+
+	len = strspn(s, accept);
+	return (len);
+}

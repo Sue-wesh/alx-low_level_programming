ceb79ec75d228da2305db1be83b9628051bd8d8d
Sue-wesh <shiroweru007@gmail.com>
2022-09-26 03:26:12 -0700

    Quiz2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,7 +5,8 @@
 /**
  * _strncpy - copy string from dest to src
  * @dest: the destination of the copying
- * src: the string we are copying
+ * @src: the string we are copying
+ * @n: no of bytes
  * Return: the result/ output
  */
 char *_strncpy(char *dest, char *src, int n)

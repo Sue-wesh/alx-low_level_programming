cbe39621473f9c39c5ad7f9d5bed4f06c7b0f07c
Sue-wesh <shiroweru007@gmail.com>
2022-09-28 14:34:11 -0700

    length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen_recursion - function finds length of string
+ * @s: string array
+ * Return: the length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return 1 + _strlen_recursion(s + 1);
+}

8a4c71398e73d633fe0d6192df55327411cbd8f8
Sue-wesh <shiroweru007@gmail.com>
2022-10-04 10:31:21 -0700

    task3

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - concatnate strings
+ * @s1: first string array
+ * @s2: second string array
+ * Return: NULL at failure
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *s3;
+
+	s3 = malloc(strlen(s1) + strlen(s2) + 1);
+
+	strcpy(s3, s1);
+	strcat(s3, s2);
+	return (s3);
+}

9336dcf07a0774f4b96c69027be16314afb45372
Sue-wesh <shiroweru007@gmail.com>
2022-10-04 08:50:41 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - return pointer to a newly allocated space
+ * @str: the string array
+ * Return: NULL if str is null
+ */
+char *_strdup(char *str)
+{
+	if (str == NULL)
+	{
+		fprintf(stderr, "insufficient space in memory");
+		return (NULL);
+	}
+	return (strdup(str));
+}

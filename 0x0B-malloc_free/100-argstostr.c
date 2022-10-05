98dbfddadf2c71f9877d0e717c522833ffec162d
Sue-wesh <shiroweru007@gmail.com>
2022-10-05 02:09:40 -0700

    concatenate

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -17,7 +17,7 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
-		str = (char*)malloc(sizeof(char) * strlen(av[i]));
+		str = (char *)malloc(sizeof(char) * strlen(av[i]));
 		strcat(str, av[i]);
 		printf("%s\n", str);
 	}

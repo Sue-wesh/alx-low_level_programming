162b95cca8fb32b98caa3cc6c6650cd7b7e1e58e
Sue-wesh <shiroweru007@gmail.com>
2022-10-06 12:00:51 -0700

    task0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function allocates memory using malloc
+ * @b: the argument taken by malloc
+ */
+void *malloc_checked(unsigned int b)
+{
+	malloc(b);
+
+	if (malloc(b) == NULL)
+		exit (98);
+	return (malloc(b));
+}

531d718b94bcdc951705659c18faafcef9046fdd
Sue-wesh <shiroweru007@gmail.com>
2022-10-11 14:25:02 -0700

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog -our structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: the owner
+ *
+ */
+typedef struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+my_dog;
+
+#endif /* DOG_H */

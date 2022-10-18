ba7c0a86f01942db12b20c8ba27bb0c4625fe4b0
Sue-wesh <111245698+Sue-wesh@users.noreply.github.com>
2022-10-18 09:34:29 +0300

    print numbers upto 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,14 +13,16 @@ void print_to_98(int n)
 		{
 			printf("%d, ", n);
 		}
-		printf("%d", n);
+		else
+			printf("%d", n);
 		n--;
 	}
 	while (n <= 98)
 	{
 		if (n != 98)
 			printf("%d, ", n);
-		printf("%d", n);
+		else
+			printf("%d", n);
 		n++;
 	}
 	putchar('\n');

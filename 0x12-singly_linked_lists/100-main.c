ed3ac4c2405e07cb8acb4f96aefb163921bd299c
Sue-wesh <shiroweru007@gmail.com>
2022-10-24 16:12:36 -0700

    task5

--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -2,7 +2,6 @@
 #include "lists.h"
 
 /**
- *
  * main - check the code
  *
  * Return: Always 0.
@@ -10,6 +9,7 @@
 int main(void)
 {
 	statement();
-	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	printf("(A tortoise, having pretty good sense of a ");
+	printf("hare's nature, challenges one to a race.)\n");
 	return (0);
 }

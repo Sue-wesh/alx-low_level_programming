8c084f207421090d0f71dc5782603a84ffc753f8
Sue-wesh <shiroweru007@gmail.com>
2022-10-24 13:03:19 -0700

    task0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * print_list - prints elements of a list
+ * @h: stands for an element in a list
+ * Return: h at the end
+ */
+size_t print_list(const list_t *h)
+{
+	if (h != NULL)
+	{
+		printf("%s", h->str);
+		h = h->next;
+	}
+	else
+		printf("%d", '\0');
+	return (0);
+}

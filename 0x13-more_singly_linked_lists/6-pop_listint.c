c671b7432b1f0ba74db5006ebf314fcfc05f2b87
Sue-wesh <shiroweru007@gmail.com>
2022-10-28 15:25:39 -0700

    task6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - delete first node of a list
+ * @head: the first element 
+ *
+ * Return: the head node’s data (n)
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *next_node = NULL;
+	int data;
+
+	next_node = (*head)->next;
+	data = (*head)->n;
+	free(*head);
+	*head = next_node;
+
+	return (data);	
+}

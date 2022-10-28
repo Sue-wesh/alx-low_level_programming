e6553c9840c3510d19c1045915a9a4f1a1a3fa7b
Sue-wesh <shiroweru007@gmail.com>
2022-10-27 23:17:53 -0700

    task2- add element at beg

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_nodeint - add node at beg of list
+ * @head: first node of list
+ * @n: value
+ * Return: address of new element
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (0);
+}

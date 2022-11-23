7a0d301a5fcd019384ba0445af5ca48cd52dfe5c
Sue-wesh <shiroweru007@gmail.com>
2022-11-23 02:46:48 -0800

    print number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * print_number - prints an integer
+ * @n: the number
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	putchar(n % 10 + '0');
+}

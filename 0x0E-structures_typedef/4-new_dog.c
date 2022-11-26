f77d4d50793a8cbdde46af6480ef8a25133d2e1b
Sue-wesh <shiroweru007@gmail.com>
2022-11-26 11:45:24 -0800

    create a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - create a new dog
+ * @name: name of the new dog
+ * @age: its age
+ * @owner: its pwner
+ * Return: null if function fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *my_dog;
+	
+	my_dog = malloc(sizeof(dog_t));
+
+	if (my_dog == NULL)
+		return (NULL);
+	my_dog->name = name;
+	my_dog->age = age;
+	my_dog->owner = owner;
+
+	return (my_dog);
+}

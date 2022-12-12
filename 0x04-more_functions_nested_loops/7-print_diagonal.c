41c55c0ce99f81a6d542fa29929306bc9ef9be10
Sue-wesh <shiroweru007@gmail.com>
2022-12-12 02:21:23 -0800

    pattern

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,13 +12,15 @@ void print_diagonal(int n)
 
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < n; j++)
+		for (j = 0; j < i; j++)
 		{
 			if (i == j)
 			{
 				putchar(92);
+				putchar('\n');
 			}
-			putchar(' ');
+			else
+				putchar(' ');
 		}
 		putchar('\n');
 	}

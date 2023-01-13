66614c421bb91efa6a764064873e2dea0f7b9aef
Sue-wesh <shiroweru007@gmail.com>
2023-01-13 06:41:06 -0800

    search for a certain key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -24,6 +24,6 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	while (element && strcmp(element->key, key) != 0)
 		/* if they dont compare keep traversing the table/array*/
 		element = element->next;
-	
+
 	return ((element == NULL) ? NULL : element->value);
 }

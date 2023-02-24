8cf00d74618aa99db3709330588bbe4a49e19088
Sue-wesh <shiroweru007@gmail.com>
2023-02-24 11:56:32 -0800

    island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""defines perimeter of an island."""
+
 
 def island_perimeter(grid):
     """
@@ -9,15 +11,16 @@ def island_perimeter(grid):
     0 rep water zone
     1 rep land zone
     """
-    
+    width = len(grid[0])
+    height = len(grid)
     perimeter = 0
 
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if (grid[i][j] == 1):
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
                 perimeter += 4
-                if (i > 0 and grid[i - 1][j]):
+                if (i > 0 and grid[i - 1][j] == 1):
                     perimeter -= 2
-                if (j > 0 and grid[i][j - 1]):
+                if (j > 0 and grid[i][j - 1] == 1):
                     perimeter -= 2
     return perimeter
